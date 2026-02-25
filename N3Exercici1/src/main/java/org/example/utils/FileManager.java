7906eb51a7353410441f06cada00845e4389dee3
JoelQuintana <joelqn95@gmail.com>
2026-02-25 13:44:00 +0100

    feat(n3exercici1): add FileManager to load initial people from CSV

---
+++ N3Exercici1/src/main/java/org/example/utils/FileManager.java
@@ -0,0 +1,34 @@
+package org.example.utils;
+
+import org.example.Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import static org.example.utils.ValidationUtils.*;
+
+
+public class FileManager {
+
+    public static HashSet<Person> loadPersonCSVtoHashSet(Path p) {
+        HashSet<Person> people = new HashSet<>();
+        try(BufferedReader br = Files.newBufferedReader(p)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isBlank()) continue;
+                String[] parts = line.split(",");
+                if (parts.length != 3 || !isValidDni(parts[2].trim())) {
+                    System.out.println("Invalid DNI - " + parts[2].trim());
+                    continue;
+                }
+                people.add(new Person(parts[0].trim(), parts[1].trim(), parts[2].trim()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return people;
+    }
+
+}
