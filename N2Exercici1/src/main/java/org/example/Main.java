4a3377d051d93378284d3c62888f0b9391337c01
JoelQuintana <joelqn95@gmail.com>
2026-02-25 11:40:12 +0100

    feat(n2exercici1): implement Restaurant HashSet uniqueness and main setup

--- N2Exercici1/src/main/java/org/example/Main.java
+++ N2Exercici1/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class Main {
     static void main() {
+        HashSet<Restaurant> restaurants = new HashSet<>();
 
+        System.out.println(restaurants.add(new Restaurant("A1",0)));
+        System.out.println(restaurants.add(new Restaurant("A1",0)));
+        System.out.println(restaurants.add(new Restaurant("A1",5)));
+        restaurants.forEach(System.out::println);
     }
 }
