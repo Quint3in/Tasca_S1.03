a749930bad1688107a1229a3be10e29ea56fe098
JoelQuintana <joelqn95@gmail.com>
2026-02-25 13:46:20 +0100

    feat(n3exercici1): add Main entrypoint and Person model

--- N3Exercici1/src/main/java/org/example/Main.java
+++ N3Exercici1/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.nio.file.Path;
+import java.util.HashSet;
+
+import static org.example.utils.FileManager.loadPersonCSVtoHashSet;
+
 public class Main {
     static void main() {
+        final String MODULE = "N3Exercici1/";
+        final String INPUT_FILE = MODULE + "persones.csv";
 
+        HashSet<Person> people = loadPersonCSVtoHashSet(Path.of(INPUT_FILE));
+
+        ConsoleUI ui = new ConsoleUI(people);
+        ui.start();
     }
 }
