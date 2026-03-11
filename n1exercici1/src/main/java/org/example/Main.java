4cc677b0d95d958e963cb577b73a73e548ab90cb
JoelQuintana <joelqn95@gmail.com>
2026-03-11 16:55:45 +0100

    fix(n1exercici1): add Month equality and iterate months with for-each

--- n1exercici1/src/main/java/org/example/Main.java
+++ n1exercici1/src/main/java/org/example/Main.java
@@ -1,23 +1,25 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
     static void main() {
-        List<Month> months = new ArrayList<>();
-
-        months.add(new Month("January"));
-        months.add(new Month("February"));
-        months.add(new Month("March"));
-        months.add(new Month("April"));
-        months.add(new Month("May"));
-        months.add(new Month("June"));
-        months.add(new Month("July"));
-        //months.add(new Month("August"));
-        months.add(new Month("September"));
-        months.add(new Month("October"));
-        months.add(new Month("November"));
-        months.add(new Month("December"));
+        List<Month> months = new ArrayList<>(
+                List.of(
+                        new Month("January"),
+                        new Month("February"),
+                        new Month("March"),
+                        new Month("April"),
+                        new Month("May"),
+                        new Month("June"),
+                        new Month("July"),
+                        new Month("September"),
+                        new Month("October"),
+                        new Month("November"),
+                        new Month("December")
+                )
+        );
 
         Month august = new Month("August");
         months.add(7,august);
@@ -33,6 +35,11 @@ public class Main {
 
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
+        }
+
+        System.out.println("-----BUCLE FOR---------");
+        for (Month month : months) {
+            System.out.println(month);
         }
     }
 }
--- n1exercici1/src/main/java/org/example/Month.java
+++ n1exercici1/src/main/java/org/example/Month.java
@@ -1,10 +1,24 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Month {
     private final String name;
 
     public Month(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(name, month.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 
     @Override
